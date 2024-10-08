f3012f039bcb09d4f404a1d9f3b95bfcab285f53
aslamkhanbhomiya <aslamkhanbhomiyaa@gmail.com>
2024-10-08 20:44:22 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".customerSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 8,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
