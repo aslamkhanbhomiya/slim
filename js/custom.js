e50611bda8f616ba5e20fd43951ef6b9c217ca02
aslamkhanbhomiya <aslamkhanbhomiyaa@gmail.com>
2024-10-11 13:21:51 +0530

    design push

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,6 @@
 var swiper = new Swiper(".customerSwiper", {
     slidesPerView: 4,
     spaceBetween: 8,
-    centeredSlides: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
